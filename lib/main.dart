43a8ace848cc4faa0c73b3e0037e76135776bc7e
Malik Rasaq <malikrasaq@gmail.com>
2023-09-11 13:27:01 +0100

    Added app assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,34 @@
+import 'package:blog_club_app/src/views/auth.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const MyApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Blog Club App'),
-        ),
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: MaterialApp(
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        title: 'BlogClub',
+        debugShowCheckedModeBanner: false,
+        // theme: AppTheme.theme,
+        home: const AuthView(),
       ),
     );
   }
---
+++ lib/src/core/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class AppColors {
+  const AppColors._();
+  static const Color darkBlue = Color(0xFF0D253C);
+  static const Color blue = Color(0xFF376AED);
+  static const Color darkGrey = Color(0xFF7B8BB2);
+  static const Color darkBlueText = Color(0xFF2D4379);
+}
