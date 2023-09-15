6c728e058c3bc7c25adb321286ca5da6dfec932e
Malik Rasaq <malikrasaq@gmail.com>
2023-09-15 18:59:47 +0100

    Added tab bar

--- lib/src/views/auth.dart
+++ lib/src/views/auth.dart
@@ -15,115 +15,272 @@ class _AuthViewState extends State<AuthView> {
   @override
   Widget build(BuildContext context) {
     SizeConfig.init(context);
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Space.height(29),
-            Image.asset(
-              ImagePath.logo,
-              height: SizeConfig.height(56),
-              width: SizeConfig.width(110),
-            ),
-            Space.height(30),
-            Container(
-              height: SizeConfig.height(575),
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                color: AppColors.white,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(28),
-                    topRight: Radius.circular(28)),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Space.height(29),
+              Image.asset(
+                ImagePath.logo,
+                height: SizeConfig.height(56),
+                width: SizeConfig.width(110),
               ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              Space.height(30),
+              Container(
+                height: SizeConfig.height(615),
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: AppColors.blue,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(28),
+                      topRight: Radius.circular(28)),
+                ),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                      'Welcome back',
-                      style: Theme.of(context).textTheme.headlineLarge,
+                    SizedBox(
+                      height: SizeConfig.height(60),
+                      child: TabBar(
+                          labelColor: AppColors.white,
+                          unselectedLabelStyle:
+                              Theme.of(context).textTheme.bodySmall,
+                          labelStyle: Theme.of(context).textTheme.bodySmall,
+                          unselectedLabelColor: Colors.white.withOpacity(0.2),
+                          indicator: const BoxDecoration(
+                            color: AppColors.blue,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(28),
+                                topRight: Radius.circular(28)),
+                          ),
+                          tabs: const [
+                            Text(
+                              'LOGIN',
+                            ),
+                            Text(
+                              'SIGN UP',
+                            ),
+                          ]),
                     ),
-                    Space.height(12),
-                    Text(
-                      'Sign in with your account',
-                      style: Theme.of(context).textTheme.bodySmall,
-                    ),
-                    Space.height(37),
-                    const BCTextField(
-                      hintText: 'Username',
-                    ),
-                    Space.height(20),
-                    const BCTextField(
-                      hintText: 'Password',
-                      isPassword: true,
-                    ),
-                    Space.height(30),
-                    Center(
-                      child: BCButtton(
-                          onTap: () {},
-                          wid: 295,
-                          hei: 60,
-                          child: Text(
-                            'Login',
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyMedium
-                                ?.copyWith(color: AppColors.white),
-                          )),
-                    ),
-                    Space.height(20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Forgot your password?',
-                          style: Theme.of(context).textTheme.bodySmall,
-                        ),
-                        Space.width(8),
-                        GestureDetector(
-                            child: Text(
-                          'Reset here',
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodySmall
-                              ?.copyWith(color: AppColors.blue),
-                        ))
-                      ],
-                    ),
-                    Space.height(32),
-                    Center(
-                      child: Text(
-                        'OR SIGN IN WITH',
-                        style: Theme.of(context).textTheme.labelLarge,
-                      ),
-                    ),
-                    Space.height(16),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        SvgPicture.asset(
-                          SvgPath.google,
-                          height: SizeConfig.height(36),
-                          width: SizeConfig.width(36),
-                        ),
-                        SvgPicture.asset(
-                          SvgPath.facebook,
-                          height: SizeConfig.height(36),
-                          width: SizeConfig.width(36),
-                        ),
-                        SvgPicture.asset(
-                          SvgPath.twitter,
-                          height: SizeConfig.height(36),
-                          width: SizeConfig.width(36),
-                        ),
-                      ],
-                    )
+                    const Expanded(
+                        child: TabBarView(children: [
+                      LoginTab(),
+                      RegisterTab(),
+                    ])),
                   ],
                 ),
               ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginTab extends StatelessWidget {
+  const LoginTab({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig.init(context);
+    return Container(
+      decoration: const BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(28), topRight: Radius.circular(28)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Space.height(30),
+            Text(
+              'Welcome back',
+              style: Theme.of(context).textTheme.headlineLarge,
             ),
+            Space.height(12),
+            Text(
+              'Sign in with your account',
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+            Space.height(37),
+            const BCTextField(
+              hintText: 'Username',
+            ),
+            Space.height(10),
+            const BCTextField(
+              hintText: 'Password',
+              isPassword: true,
+            ),
+            Space.height(30),
+            Center(
+              child: BCButtton(
+                  onTap: () {},
+                  wid: 295,
+                  hei: 60,
+                  child: Text(
+                    'Login',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium
+                        ?.copyWith(color: AppColors.white),
+                  )),
+            ),
+            Space.height(20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Forgot your password?',
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+                Space.width(8),
+                GestureDetector(
+                    child: Text(
+                  'Reset here',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodySmall
+                      ?.copyWith(color: AppColors.blue),
+                ))
+              ],
+            ),
+            Space.height(32),
+            Center(
+              child: Text(
+                'OR SIGN IN WITH',
+                style: Theme.of(context).textTheme.labelLarge,
+              ),
+            ),
+            Space.height(16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SvgPicture.asset(
+                  SvgPath.google,
+                  height: SizeConfig.height(36),
+                  width: SizeConfig.width(36),
+                ),
+                SvgPicture.asset(
+                  SvgPath.facebook,
+                  height: SizeConfig.height(36),
+                  width: SizeConfig.width(36),
+                ),
+                SvgPicture.asset(
+                  SvgPath.twitter,
+                  height: SizeConfig.height(36),
+                  width: SizeConfig.width(36),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RegisterTab extends StatelessWidget {
+  const RegisterTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig.init(context);
+    return Container(
+      decoration: const BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(28), topRight: Radius.circular(28)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Space.height(30),
+            Text(
+              'Hello👋',
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            Space.height(12),
+            Text(
+              'Register a new account',
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+            Space.height(37),
+            const BCTextField(
+              hintText: 'Username',
+            ),
+            Space.height(10),
+            const BCTextField(
+              hintText: 'Password',
+              isPassword: true,
+            ),
+            Space.height(30),
+            Center(
+              child: BCButtton(
+                  onTap: () {},
+                  wid: 295,
+                  hei: 60,
+                  child: Text(
+                    'Register',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyMedium
+                        ?.copyWith(color: AppColors.white),
+                  )),
+            ),
+            Space.height(20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Forgot your password?',
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+                Space.width(8),
+                GestureDetector(
+                    child: Text(
+                  'Reset here',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodySmall
+                      ?.copyWith(color: AppColors.blue),
+                ))
+              ],
+            ),
+            Space.height(32),
+            Center(
+              child: Text(
+                'OR SIGN UP WITH',
+                style: Theme.of(context).textTheme.labelLarge,
+              ),
+            ),
+            Space.height(16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SvgPicture.asset(
+                  SvgPath.google,
+                  height: SizeConfig.height(36),
+                  width: SizeConfig.width(36),
+                ),
+                SvgPicture.asset(
+                  SvgPath.facebook,
+                  height: SizeConfig.height(36),
+                  width: SizeConfig.width(36),
+                ),
+                SvgPicture.asset(
+                  SvgPath.twitter,
+                  height: SizeConfig.height(36),
+                  width: SizeConfig.width(36),
+                ),
+              ],
+            )
           ],
         ),
       ),
